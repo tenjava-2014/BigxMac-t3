1d0175439d8808a792399f1a74cbc1104c8d2291
Jared S <bigmacdev@gmail.com>
2014-07-12 14:28:27 -0400

    When player hurts pig player loc is displayed
    TODO
    -spawn mob there
    -add all non hostile mobs
    
    -THINK OF MORE FRICKIN IDEAS!

--- src/main/java/com/tenjava/entries/BigxMac/t3/PlayerListener.java
+++ src/main/java/com/tenjava/entries/BigxMac/t3/PlayerListener.java
@@ -1,6 +1,10 @@
 package com.tenjava.entries.BigxMac.t3;
 
+import java.util.Random;
+
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,13 +18,26 @@ public class PlayerListener implements Listener
 	public void ifHurtsnonHostile(EntityDamageByEntityEvent event){
 		if(event.getDamager() instanceof Player){
 			if(event.getCause() == DamageCause.ENTITY_ATTACK){
-				Bukkit.broadcastMessage("test");
-				
+				if(event.getEntityType() == EntityType.PIG){
+				Random rand = new Random();
+				int number = 0;
+				for(int i=1;i<=100;++i){
+					
+					number = 1+rand.nextInt(100);
+					
+				}
+				//TODO ADD CONFIG THAT ALLOWS CHANGE!
+				if(number <= 100){
+					
+					Location loc = event.getDamager().getLocation();
+					Bukkit.broadcastMessage(loc + "");
+					
+				}
 			}
 			
 			
 		}
-		
+		}
 		
 	}
 	
