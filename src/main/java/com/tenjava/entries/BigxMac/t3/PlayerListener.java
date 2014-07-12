820e3094bb5732fe01cc90e3af7b773c83ec1771
Jared S <bigmacdev@gmail.com>
2014-07-12 10:21:14 -0400

    Added -onEnable -onDisable -Listener -Theme and name

---
+++ src/main/java/com/tenjava/entries/BigxMac/t3/PlayerListener.java
@@ -0,0 +1,19 @@
+package com.tenjava.entries.BigxMac.t3;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.weather.WeatherEvent;
+
+public class PlayerListener implements Listener 
+{
+
+	@EventHandler
+	public void whenRaining(WeatherEvent event)
+	{
+		
+		
+		
+	}
+	
+	
+}
