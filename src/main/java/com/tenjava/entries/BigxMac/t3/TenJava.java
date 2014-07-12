971f0470a158fe8c952caec2971aa5cbe2aec840
Jared S <bigmacdev@gmail.com>
2014-07-12 10:14:10 -0400

    Added onEnable, onDisable, Theme, and Name!

--- src/main/java/com/tenjava/entries/BigxMac/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/BigxMac/t3/TenJava.java
@@ -2,4 +2,27 @@ package com.tenjava.entries.BigxMac.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+	
+	@Override
+	public void onEnable(){
+		
+		getLogger().info("Wild Water has been enabled!");
+		
+		
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+		getLogger().info("is currently disabled!");
+		
+		
+	}
+	
+	
+	
+	
+	
+}
