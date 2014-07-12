ada4f9506fbf22f0c30b40aaa4befc7ad5659b74
Jared S <bigmacdev@gmail.com>
2014-07-12 16:51:06 -0400

    Added Creepers 100% Hmm Next Skeletons???

--- src/main/java/com/tenjava/entries/BigxMac/t3/PlayerListener.java
+++ src/main/java/com/tenjava/entries/BigxMac/t3/PlayerListener.java
@@ -4,13 +4,14 @@ import java.util.Random;
 
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
-import org.bukkit.event.entity.EntityDeathEvent;
 
 public class PlayerListener implements Listener 
 {
@@ -29,15 +30,15 @@ public class PlayerListener implements Listener
 						event.getEntityType() == EntityType.COW ||
 						event.getEntityType() == EntityType.SHEEP ||
 						event.getEntityType() == EntityType.BAT ||
-						event.getEntityType() == EntityType.CHICKEN
+						event.getEntityType() == EntityType.CHICKEN)
 						
-						){
+				{
 				// Random Number Generator
-				Random rand = new Random();
-				int number = 0;
-				for(int i=1;i<=100;++i){
+					Random rand = new Random();
+					int number = 0;
+					for(int i=1;i<=100;++i){
 					
-				number = 1+rand.nextInt(100);
+					number = 1+rand.nextInt(100);
 					
 				}
 				
@@ -49,40 +50,46 @@ public class PlayerListener implements Listener
 					World world = event.getDamager().getWorld();
 					world.spawnEntity(loc, EntityType.ZOMBIE);
 					
-						}
 					}
 				}
-				
+			}
 		}
 	}
 	
-	public void onCreeperkill(EntityDeathEvent event)
-	{
-		if(TenJava.getInstance().getConfig().getBoolean("enabled"))
-		{
-			if(event.getEntityType() == EntityType.CREEPER)
-			{
+	
+	
+	
+	@EventHandler
+	public void hurtsCreeper(EntityDamageByEntityEvent event){
+		if(TenJava.getInstance().getConfig().getBoolean("enabled") && event.getDamager() instanceof Player && 
 				
+				(event.getCause() == DamageCause.ENTITY_ATTACK && event.getEntityType() == EntityType.CREEPER &&
+				
+				event.getDamage() <= 12))
+		{
+			
+		
 				Random rand = new Random();
 				int number = 0;
 				for(int i=1;i<=100;++i){
 					
 				number = 1+rand.nextInt(100);
+					
+				
+				 
 				if(number <= TenJava.instance.getConfig().getInt("CreeperChance")){
 					
+					Location creeper = event.getEntity().getLocation();
+					Entity tnt = event.getEntity().getLocation().getWorld().spawnEntity(creeper, EntityType.PRIMED_TNT);
+					((TNTPrimed)tnt).setFuseTicks(0);
 					
-					
-						}
 				}
-				
-				
 			}
-			
-			
 		}
-		
-		
 	}
+	
+	
+	
 	
 	
 	
