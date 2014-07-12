ce4165e999622a4aa3ea8d3f0d8c4e2df6bc907c
Jared S <bigmacdev@gmail.com>
2014-07-12 11:36:00 -0400

    If player is on ground and its been raining for more than 3 secs 60ticks say its raining(its basic... I know  im just starting)

--- src/main/java/com/tenjava/entries/BigxMac/t3/PlayerListener.java
+++ src/main/java/com/tenjava/entries/BigxMac/t3/PlayerListener.java
@@ -1,19 +1,43 @@
 package com.tenjava.entries.BigxMac.t3;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.weather.WeatherEvent;
 
 public class PlayerListener implements Listener 
 {
 
+	Boolean storm = false;
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void onPlayerJoin(PlayerInteractEvent event)
+	{
+		
+		Player player = event.getPlayer();
+		 // Location loc = player.getLocation();
+		
+		if(storm){
+			if(player.isOnGround()){
+				
+				
+				
+			
+			 
+			player.sendMessage("Its raining!");
+		}
+		}
+		
+		
+	}
+	
 	@EventHandler
 	public void whenRaining(WeatherEvent event)
 	{
+
 	
-		
-	
-		
 		if(event.getWorld().hasStorm())
 		{
 			
@@ -21,6 +45,7 @@ public class PlayerListener implements Listener
 			//TODO CHANGE TIME TO MORE TICKS (20tps) 3 secs is for demo!
 			if(event.getWorld().getWeatherDuration() <=  60){
 				
+				boolean storm = true;
 				
 				
 				
@@ -28,10 +53,16 @@ public class PlayerListener implements Listener
 			
 			
 			
-		}else{}
+		}else{
+			
+			
+			
+		}
+		
 		
 		
 	}
+	
 	
 	
 }
